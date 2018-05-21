4d5b03d8fbfff120fe9abe0ebe148806bea4cd83
駒井宏光 <hiro.koma.ghq@softbank.ne.jp>
2018-05-21 15:27:23 +0900

    first commit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < RankingController
 
-  before_action :authenticate_user!,only: :newp
+  before_action :authenticate_user!,only: :new
   def new
     @product = Product.find(params[:product_id])
     @review = Review.new
